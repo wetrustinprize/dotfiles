ae782e90b9f8c63e5d58208fd9d2e5894d131a07
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-08-13 09:27:13 -0300

    show gpu usage %

---
+++ configs/polybar/scripts/system-nvidia-smi.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# https://github.com/polybar/polybar-scripts/tree/master/polybar-scripts/system-nvidia-smi
+
+nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print ""$1""}'
