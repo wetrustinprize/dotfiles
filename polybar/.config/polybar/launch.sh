730a32f61df0ee59d69c6ff27efa5e3ff45f87ea
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-12-03 00:22:01 -0300

    i3 and polybar changes

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
-done
+polybar --reload primary-top &
+polybar --reload primary-bottom &
+
+polybar --reload secondary-top &
+polybar --reload secondary-bottom
 
 echo "Bars launched..."
