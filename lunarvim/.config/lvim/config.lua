af3975a6f2d4a5701df5e4cb77c402c9a9eb3d4f
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-12-03 14:55:36 -0300

    changes to lunarvim

--- lunarvim/.config/lvim/config.lua
+++ lunarvim/.config/lvim/config.lua
@@ -11,28 +11,28 @@ lvim.lsp.buffer_mappings.normal_mode["H"] = { ":HopWord<CR>", "Hop word" }
 
 -- scribe
 lvim.builtin.which_key.mappings["n"] = {
-	name = "Notes",
-	n = { ":ScribeOpen<cr>", "Default note" },
-	N = { ":ScribeNew<cr>", "New note" },
-	f = { ":ScribeFind<cr>", "Find note" },
+  name = "Notes",
+  n = { ":ScribeOpen<cr>", "Default note" },
+  N = { ":ScribeNew<cr>", "New note" },
+  f = { ":ScribeFind<cr>", "Find note" },
 }
 
 -- PARSERS
 lvim.builtin.treesitter.ensure_installed = {
-	"bash",
-	"javascript",
-	"json",
-	"lua",
-	"python",
-	"typescript",
-	"tsx",
-	"css",
-	"scss",
-	"rust",
-	"yaml",
-	"haskell",
-	"prisma",
-	"graphql",
+  "bash",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "scss",
+  "rust",
+  "yaml",
+  "haskell",
+  "prisma",
+  "graphql",
 }
 
 lvim.builtin.treesitter.ignore_install = {}
@@ -40,83 +40,89 @@ lvim.builtin.treesitter.highlight.enable = true
 
 -- LSP SETTINGS
 lvim.lsp.installer.setup.ensure_installed = {
-	"sumneko_lua",
-	"jsonls",
+  "sumneko_lua",
+  "jsonls",
 
-	-- Node
-	"tsserver",
-	"eslint",
-	"tailwindcss",
+  -- Node
+  "tsserver",
+  "eslint",
+  "tailwindcss",
 
-	"prismals",
-	"omnisharp",
+  "prismals",
+  "omnisharp",
 }
 
 -- FORMATTERS AND LINTERS
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
-	{
-		command = "eslint_d",
-		filetypes = { "typescript", "typescriptreact" },
-	},
-	{
-		command = "write_good",
-	},
+  {
+    command = "eslint_d",
+    filetypes = { "typescript", "typescriptreact" },
+  },
+  {
+    command = "write_good",
+  },
 })
 
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-	{
-		command = "eslint_d",
-		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-	},
-	{
-		command = "prettierd",
-		filetypes = { "markdown", "markdown.mdx", "graphql", "css", "scss" },
-	},
-	{
-		command = "stylua",
-		filetype = { "lua" },
-	},
+  {
+    command = "eslint_d",
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  },
+  {
+    command = "prettierd",
+    filetypes = { "markdown", "markdown.mdx", "graphql", "css", "scss" },
+  },
+  {
+    command = "stylua",
+    filetype = { "lua" },
+  },
 })
 
 -- PLUGINS
 lvim.plugins = {
-	-- {
-	--   "folke/trouble.nvim",
-	--   cmd = "TroubleToggle",
-	-- },
-	{ "arcticicestudio/nord-vim" },
-	{
-		"andweeb/presence.nvim",
-		config = function()
-			require("presence"):setup({
-				main_image = "file",
-			})
-		end,
-	},
-	{ "christoomey/vim-tmux-navigator" },
-	{ "MunifTanjim/nui.nvim" },
-	{
-		"wetrustinprize/scribe.nvim",
-		config = function()
-			require("scribe").setup({})
-		end,
-	},
-	{
-		"karb94/neoscroll.nvim",
-		event = "WinScrolled",
-		config = function()
-			require("neoscroll").setup({
-				mappings = { "<C-u>", "<C-d>" },
-			})
-		end,
-	},
-	{
-		"phaazon/hop.nvim",
-		event = "BufRead",
-		config = function()
-			require("hop").setup()
-		end,
-	},
+  -- {
+  --   "folke/trouble.nvim",
+  --   cmd = "TroubleToggle",
+  -- },
+  { "arcticicestudio/nord-vim" },
+  {
+    "andweeb/presence.nvim",
+    config = function()
+      require("presence"):setup({
+        main_image = "file",
+      })
+    end,
+  },
+  { "christoomey/vim-tmux-navigator" },
+  { "MunifTanjim/nui.nvim" },
+  -- { "ThePrimeagen/vim-be-good" },
+  { "nkakouros-original/numbers.nvim",
+    config = function()
+      require("numbers").setup({})
+    end
+  },
+  {
+    "wetrustinprize/scribe.nvim",
+    config = function()
+      require("scribe").setup({})
+    end,
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require("neoscroll").setup({
+        mappings = { "<C-u>", "<C-d>" },
+      })
+    end,
+  },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+    end,
+  },
 }
