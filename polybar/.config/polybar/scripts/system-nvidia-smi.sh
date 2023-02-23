75c6af45cceabb3431df16daf06b00b458e49409
Peterson Adami Candido <contact.petersonac@protonmail.com>
2023-02-23 11:29:40 -0300

    feat: error handling

--- polybar/.config/polybar/scripts/system-nvidia-smi.sh
+++ polybar/.config/polybar/scripts/system-nvidia-smi.sh
@@ -3,6 +3,7 @@
 # customization variables
 color_used="%{F#eceff4}"
 color_unused="%{F#4c566a}"
+color_error="%{F#bf616a}"
 
 char_fill=""
 char_empty=""
@@ -14,19 +15,24 @@ total_bars=4
 # https://github.com/polybar/polybar-scripts/tree/master/polybar-scripts/system-nvidia-smi
 percentage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
 
-result=""
-pieces=$((percentage/(100/total_bars)))
+if [ $? -ne 0 ]; then
+    result="nvidia-smi error"
+    echo "$color_error$result%{F-}"
+else
+    result=""
+    pieces=$((percentage/(100/total_bars)))
 
-for i in $(seq 1 ${total_bars})
-do
-    if [ $pieces -eq 0 ]; then
-        result="$result$color_unused$char_empty"
-    elif [ $pieces -ge $i ] || [ $pieces -eq $total_bars ]; then
-        result="$result$color_used$char_fill"
-    else
-        result="$result$color_unused$char_empty"
-    fi
-done
+    for i in $(seq 1 ${total_bars})
+    do
+        if [ $pieces -eq 0 ]; then
+            result="$result$color_unused$char_empty"
+        elif [ $pieces -ge $i ] || [ $pieces -eq $total_bars ]; then
+            result="$result$color_used$char_fill"
+        else
+            result="$result$color_unused$char_empty"
+        fi
+    done
 
-result="$result%{F-}"
-echo $result
+    result="$result%{F-}"
+    echo $result
+fi
