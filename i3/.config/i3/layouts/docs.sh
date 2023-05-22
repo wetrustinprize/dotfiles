c02d630a70fcdddfeac1d41dc2a2739edeead6aa
Prize <me@wetrustinprize.com>
2023-05-22 11:58:18 -0300

    fix(i3): layouts

--- i3/.config/i3/layouts/docs.sh
+++ i3/.config/i3/layouts/docs.sh
@@ -4,4 +4,4 @@ i3-msg "workspace 3:doc; append_layout ~/.config/i3/layouts/workspace_doc.json"
 
 # And finally we fill the containers with the programs they had
 (slack & disown)
-(thunderbird & disown)
+(morgen & disown)
