7656e19f3c376a2bd4fe23de6c8babf6a573b56b
Prize <me@wetrustinprize.com>
2023-05-29 18:55:03 -0300

    fix(i3): changed social workspace

--- i3/.config/i3/layouts/soc.sh
+++ i3/.config/i3/layouts/soc.sh
@@ -2,7 +2,7 @@
 
 i3-msg "workspace 4:soc; append_layout ~/.config/i3/layouts/workspace_soc.json"
 
-# And finally we fill the containers with the programs they had
 (whatsapp-nativefier & disown)
 (discord & disown)
 (telegram-desktop & disown)
+(slack & disown)
