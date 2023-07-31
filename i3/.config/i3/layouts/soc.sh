0c15670a3d36fb8be4e39d89b290383f355ccef5
Peterson Adami Candido <me@wetrustinprize.com>
2023-07-31 20:48:18 -0300

    fix: removed whatsapp and telegram

--- i3/.config/i3/layouts/soc.sh
+++ i3/.config/i3/layouts/soc.sh
@@ -2,7 +2,5 @@
 
 i3-msg "workspace 4:soc; append_layout ~/.config/i3/layouts/workspace_soc.json"
 
-(whatsapp-nativefier & disown)
 (discord & disown)
-(telegram-desktop & disown)
 (slack & disown)
